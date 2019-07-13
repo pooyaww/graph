7e40c7a4ff86c04e691e04c0f1d9e04685e9b7b6
Puya Amiri <pooya.w@gmail.com>
2019-07-13 02:32:16 +0200

    fix

--- topol_sort_indegree/topol_sort_indegree.c
+++ topol_sort_indegree/topol_sort_indegree.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 typedef struct {
     unsigned int from;
@@ -7,24 +8,24 @@ typedef struct {
 } dependence;
 
 /* Find out if a kernel has no dependency (no input from other kernels to this one) */
-static unsigned int is_free_kernel(const dependence *dependencies, const unsigned int *from_to, unsigned int num_of_dependencies,
+static bool is_free_kernel(const dependence *dependencies, bool *dependency_bool_vector, unsigned int num_of_dependencies,
         unsigned int kernel)
 {
     unsigned int from_this_kernel, free_kernel = 1;
     for (from_this_kernel = 0; from_this_kernel < num_of_dependencies && free_kernel; from_this_kernel++) {
-        free_kernel = !from_to[from_this_kernel] || dependencies[from_this_kernel].to != kernel;
+        free_kernel = !dependency_bool_vector[from_this_kernel] || dependencies[from_this_kernel].to != kernel;
     }
     return free_kernel;
 }
 
 /* Get the kernels with no dependency (no input from other kernels to those) */
-static unsigned int get_free_kernels(const dependence *dependencies, const unsigned int *from_to, unsigned int num_of_dependencies,
-        unsigned int num_of_kernels, unsigned int *free_kernels)
+static unsigned int get_free_kernels(const dependence *dependencies, bool *dependency_bool_vector, unsigned int num_of_dependencies,
+        unsigned int num_of_kernels, bool *free_kernels_bool_vector)
 {
     unsigned int kernel, num_of_free_kernels = 0;
     for (kernel = 0; kernel < num_of_kernels; kernel++) {
-        if (is_free_kernel(dependencies, from_to, num_of_dependencies, kernel)) {
-            free_kernels[kernel] = 1;
+        if (is_free_kernel(dependencies, dependency_bool_vector, num_of_dependencies, kernel)) {
+            free_kernels_bool_vector[kernel] = 1;
             num_of_free_kernels++;
         }
     }
@@ -32,11 +33,11 @@ static unsigned int get_free_kernels(const dependence *dependencies, const unsig
 }
 
 unsigned int topological_sort(const dependence *dependencies, unsigned int num_of_dependencies, unsigned int num_of_kernels,
-        unsigned int **sorted)
+        unsigned int **resolved)
 {
-    unsigned int *free_kernels = calloc(num_of_kernels, sizeof(unsigned int));
-    unsigned int *boolean_vector_of_dependencies = malloc(num_of_dependencies * sizeof(unsigned int));
-    *sorted = malloc( num_of_kernels* sizeof(unsigned int));
+    bool *free_kernels_bool_vector = calloc(num_of_kernels, sizeof(bool));
+    bool *dependency_bool_vector = malloc(num_of_dependencies * sizeof(bool));
+    *resolved = malloc( num_of_kernels* sizeof(unsigned int));
     unsigned int kernel, a, num_of_free_kernels, sorted_size = 0,
             arcs_size = num_of_dependencies;
 //    if (!(vertices && arcs && *sorted)) {
@@ -48,35 +49,35 @@ unsigned int topological_sort(const dependence *dependencies, unsigned int num_o
 //    }
     /* All arcs start off in the graph */
     for (a = 0; a < num_of_dependencies; a++) {
-        boolean_vector_of_dependencies[a] = 1;
+        dependency_bool_vector[a] = 1;
     }
     /* Get the vertices with no incoming edges */
-    num_of_free_kernels = get_free_kernels(dependencies, boolean_vector_of_dependencies, num_of_dependencies, num_of_kernels, free_kernels);
+    num_of_free_kernels = get_free_kernels(dependencies, dependency_bool_vector, num_of_dependencies, num_of_kernels, free_kernels_bool_vector);
     /* Main loop */
     while (num_of_free_kernels > 0) {
         /* Get first vertex */
-        for (kernel = 0; free_kernels[kernel] != 1; kernel++);
+        for (kernel = 0; free_kernels_bool_vector[kernel] != 1; kernel++);
         /* Remove from vertex set */
-        free_kernels[kernel] = 0;
+        free_kernels_bool_vector[kernel] = 0;
         num_of_free_kernels--;
         /* Add it to the sorted array */
-        (*sorted)[sorted_size++] = kernel;
+        (*resolved)[sorted_size++] = kernel;
         /* Remove all arcs connecting it to its neighbours */
         for (a = 0; a < num_of_dependencies; a++) {
-            if (boolean_vector_of_dependencies[a] && dependencies[a].from== kernel) {
-                boolean_vector_of_dependencies[a] = 0;
+            if (dependency_bool_vector[a] && dependencies[a].from== kernel) {
+                dependency_bool_vector[a] = 0;
                 arcs_size--;
                 /* Check if neighbour is now a root */
-                if (is_free_kernel(dependencies, boolean_vector_of_dependencies, num_of_dependencies, dependencies[a].to)) {
+                if (is_free_kernel(dependencies, dependency_bool_vector, num_of_dependencies, dependencies[a].to)) {
                     /* Add it to set of vertices */
-                    free_kernels[dependencies[a].to] = 1;
+                    free_kernels_bool_vector[dependencies[a].to] = 1;
                     num_of_free_kernels++;
                 }
             }
         }
     }
-    free(free_kernels);
-    free(boolean_vector_of_dependencies);
+    free(free_kernels_bool_vector);
+    free(dependency_bool_vector);
     return arcs_size == 0;
 }
 
@@ -93,33 +94,33 @@ void kernel_dependency(dependence *from_to, unsigned int from, unsigned int to,
 
 int main(void)
 {
-    const unsigned int size = 8; /* number of dependencies */
-    const unsigned int order = 8; /* number of kernels */
-    dependence *from_to = malloc(size * sizeof(dependence)); // Array for kernel dependencies
+    const unsigned int num_of_dependencies = 8; /* number of dependencies */
+    const unsigned int num_of_kernels = 8; /* number of kernels */
+    dependence *dependencies = malloc(num_of_dependencies * sizeof(dependence)); // Array for kernel dependencies
     unsigned int i = 0;
-    unsigned int *sorted;
+    unsigned int *resolved;
     unsigned int acyclic;
 
-    //Making dependencies (making an array of kernel dependencies name from_to)
-    kernel_dependency(from_to, 0, 2, &i);
-    kernel_dependency(from_to, 1, 3, &i);
-    kernel_dependency(from_to, 2, 3, &i);
-    kernel_dependency(from_to, 2, 4, &i);
-    kernel_dependency(from_to, 2, 5, &i);
-    kernel_dependency(from_to, 3, 6, &i);
-    kernel_dependency(from_to, 5, 7, &i);
-    kernel_dependency(from_to, 5, 7, &i);
+    //Making dependencies (making an array of kernel dependencies)
+    kernel_dependency(dependencies, 0, 2, &i);
+    kernel_dependency(dependencies, 1, 3, &i);
+    kernel_dependency(dependencies, 2, 3, &i);
+    kernel_dependency(dependencies, 2, 4, &i);
+    kernel_dependency(dependencies, 2, 5, &i);
+    kernel_dependency(dependencies, 3, 6, &i);
+    kernel_dependency(dependencies, 5, 7, &i);
+    kernel_dependency(dependencies, 5, 7, &i);
 
-    //Passing array of dependencies 
-    acyclic = topological_sort(from_to, size, order, &sorted);
-    printf("Graph is acyclic: %u\n", acyclic);
-    for (i = 0; i < order; i++) {
-        printf("%u ", sorted[i]);
+    //Passing array of dependencies
+    acyclic = topological_sort(dependencies, num_of_dependencies, num_of_kernels, &resolved);
+    printf("No cycle between kernels: %u\n", acyclic);
+    for (i = 0; i < num_of_kernels; i++) {
+        printf("%u ", resolved[i]);
     }
     putchar('\n');
 
-    free(sorted);
-    free(from_to);
+    free(resolved);
+    free(dependencies);
 
     return 0;
 }
