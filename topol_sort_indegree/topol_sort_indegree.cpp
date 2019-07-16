67c10ff95f0050e43fc6e2f9ac0aeb83c87035eb
Puya <pooya.w@gmail.com>
2019-07-16 17:57:49 +0200

    re-implemented with new data structures

--- topol_sort_indegree/topol_sort_indegree.cpp
+++ topol_sort_indegree/topol_sort_indegree.cpp
@@ -1,120 +1,108 @@
 #include <iostream>
+#include <vector>
+#include <utility>
+#include <array>
 
-typedef struct {
-    unsigned int from;
-    unsigned int to;
-} dependence;
+using Dependence = std::pair<size_t, size_t>; // <from, to>
 
 /* Find out if a kernel has no dependency (no input from other kernels to this one) */
-static bool is_free_kernel(const dependence *dependencies, bool *dependency_bool_vector, unsigned int num_of_dependencies,
-                           unsigned int kernel) {
-    unsigned int from_this_kernel, free_kernel = 1;
-    for (from_this_kernel = 0; from_this_kernel < num_of_dependencies && free_kernel; from_this_kernel++) {
-        free_kernel = !dependency_bool_vector[from_this_kernel] || dependencies[from_this_kernel].to != kernel;
+static bool is_free_kernel(std::vector<Dependence>& dependencies, bool* dependency_bool_vector, size_t num_of_dependencies,
+                           size_t kernel) {
+    size_t free_kernel = 1;
+    for (auto dependence : dependencies) {
+        if (free_kernel) {
+            free_kernel = std::get<1>(dependence) != kernel;
+            for (int i = 0; i < num_of_dependencies; i++) {
+                free_kernel = !dependency_bool_vector[i];
+            }
+        }
     }
     return free_kernel;
 }
 
 /* Get the kernels with no dependency (no input from other kernels to those) */
-static size_t get_free_kernels(const dependence *dependencies, bool *dependency_bool_vector, unsigned int num_of_dependencies,
-                                     unsigned int num_of_kernels, bool *free_kernels_bool_vector) {
-    unsigned int kernel, num_of_free_kernels = 0;
-    for (kernel = 0; kernel < num_of_kernels; kernel++) {
+static size_t get_free_kernels(std::vector<Dependence>& dependencies, bool* dependency_bool_vector , size_t num_of_dependencies,
+                               size_t num_of_kernels, bool* free_kernels_bool_vector) {
+    size_t kernel, num_of_free_kernels = 0;
+    for (kernel = 0; kernel< num_of_kernels; kernel++) {
         if (is_free_kernel(dependencies, dependency_bool_vector, num_of_dependencies, kernel)) {
-            free_kernels_bool_vector[kernel] = 1;
+            free_kernels_bool_vector[kernel] = true;
             num_of_free_kernels++;
         }
     }
     return num_of_free_kernels;
 }
 
-size_t topological_sort(const dependence *dependencies, unsigned int num_of_dependencies, unsigned int num_of_kernels,
-                              unsigned int **resolved) {
-    bool *free_kernels_bool_vector = (bool*) calloc(num_of_kernels, sizeof(bool));
-    bool *dependency_bool_vector = (bool*) malloc(num_of_dependencies * sizeof(bool));
-    *resolved = (unsigned int *)malloc( num_of_kernels* sizeof(unsigned int));
-    unsigned int kernel, a, num_of_free_kernels, resolved_size = 0,
-                 arcs_size = num_of_dependencies;
-    if (!(free_kernels_bool_vector && dependency_bool_vector && *resolved)) {
-        free(free_kernels_bool_vector);
-        free(dependency_bool_vector);
-        free(*resolved);
-        *resolved = NULL;
-        return 0;
-    }
+size_t topological_sort(std::vector<Dependence> dependencies, size_t num_of_dependencies, size_t num_of_kernels, std::vector<size_t>& resolved) {
+    bool free_kernels_bool_vector[num_of_kernels];
+    bool dependency_bool_vector[num_of_dependencies];
+    //std::vector<size_t> resolved;
+    size_t kernel, a, num_of_free_kernels, resolved_size = 0,
+           remaining_dependencies = num_of_dependencies;
+
     /* All arcs start off in the graph */
-    for (a = 0; a < num_of_dependencies; a++) {
-        dependency_bool_vector[a] = 1;
+    for (auto elem : dependency_bool_vector ) {
+        elem = true;
     }
-    /* Get the vertices with no incoming edges */
+    /* Get the kernels with no incoming dependencies */
     num_of_free_kernels = get_free_kernels(dependencies, dependency_bool_vector, num_of_dependencies, num_of_kernels, free_kernels_bool_vector);
     /* Main loop */
     while (num_of_free_kernels > 0) {
-        /* Get first vertex */
+        /* Get first kernel */
         for (kernel = 0; free_kernels_bool_vector[kernel] != 1; kernel++);
-        /* Remove from free_kernels boolian vector */
-        free_kernels_bool_vector[kernel] = 0;
+        /* Remove from free_kernels boolean vector */
+        free_kernels_bool_vector[kernel] = false;
         num_of_free_kernels--;
         /* Add it to the resolved array */
-        (*resolved)[resolved_size++] = kernel;
+        resolved.emplace_back(kernel);
+        resolved_size++;
         /* Remove all arcs connecting it to its neighbours */
         for (a = 0; a < num_of_dependencies; a++) {
-            if (dependency_bool_vector[a] && dependencies[a].from == kernel) {
-                dependency_bool_vector[a] = 0;
-                arcs_size--;
+            //for (auto elem : dependency_bool_vector) {
+            if (dependency_bool_vector[a] && std::get<0>(dependencies[a]) == kernel) {
+                dependency_bool_vector[a] = false;
+                remaining_dependencies--;
                 /* Check if neighbour is now a root */
-                if (is_free_kernel(dependencies, dependency_bool_vector, num_of_dependencies, dependencies[a].to)) {
+                if (is_free_kernel(dependencies, dependency_bool_vector, num_of_dependencies, std::get<1>(dependencies[a]))) {
                     /* Add it to set of vertices */
-                    free_kernels_bool_vector[dependencies[a].to] = 1;
+                    free_kernels_bool_vector[std::get<1>(dependencies[a])] = 1;
                     num_of_free_kernels++;
                 }
             }
         }
+        }
+        return remaining_dependencies == 0;
     }
-    free(free_kernels_bool_vector);
-    free(dependency_bool_vector);
-    return arcs_size == 0;
-}
 
-
-/* Connect two arcs */
-// we store thre arcs connections instead of conventional methods which keep tracks of nodes in adj-list
-void kernel_dependency(dependence *from_to, unsigned int from, unsigned int to,
-                       unsigned int *index) {
-    from_to[*index].from = from;
-    from_to[*index].to = to;
-    (*index)++;
-}
-
-int main(void) {
-    const unsigned int num_of_dependencies = 8; /* number of dependencies */
-    const unsigned int num_of_kernels = 8; /* number of kernels */
-    dependence *dependencies = (dependence*) malloc(num_of_dependencies * sizeof(dependence)); // Array for kernel dependencies
-    unsigned int i = 0;
-    unsigned int *resolved;
-    unsigned int acyclic;
-
-    // Dependencies should be determined by an analyse phase on kernels
-    // Making dependencies (making an array of kernel dependencies)
-    kernel_dependency(dependencies, 0, 2, &i);
-    kernel_dependency(dependencies, 1, 3, &i);
-    kernel_dependency(dependencies, 2, 3, &i);
-    kernel_dependency(dependencies, 2, 4, &i);
-    kernel_dependency(dependencies, 2, 5, &i);
-    kernel_dependency(dependencies, 3, 6, &i);
-    kernel_dependency(dependencies, 5, 7, &i);
-    kernel_dependency(dependencies, 5, 7, &i);
-
-    //Passing array of dependencies
-    acyclic = topological_sort(dependencies, num_of_dependencies, num_of_kernels, &resolved);
-    std::cout<< "No cycle between kernels: "<< acyclic;
-    for (i = 0; i < num_of_kernels; i++) {
-        printf("%u ", resolved[i]);
+    void kernel_dependency(std::vector<Dependence>& dependence, size_t from, size_t to) {
+        dependence.emplace_back(from,to);
     }
-    putchar('\n');
 
-    free(resolved);
-    free(dependencies);
+    int main(void) {
+        const size_t num_of_dependencies = 8; /* number of dependencies */
+        const size_t num_of_kernels = 8; /* number of kernels */
+        std::vector<Dependence> dependencies; // vector for kernel dependencies
+        std::vector<size_t> resolved;
+        size_t acyclic;
 
-    return 0;
-}
+        // Dependencies should be determined by an analyse phase on kernels
+        // Making dependencies (making an array of kernel dependencies)
+        kernel_dependency(dependencies, 0, 2);
+        kernel_dependency(dependencies, 1, 3);
+        kernel_dependency(dependencies, 2, 3);
+        kernel_dependency(dependencies, 2, 4);
+        kernel_dependency(dependencies, 2, 5);
+        kernel_dependency(dependencies, 3, 6);
+        kernel_dependency(dependencies, 5, 7);
+        kernel_dependency(dependencies, 5, 7);
+
+        //Passing vector of dependencies
+        acyclic = topological_sort(dependencies, num_of_dependencies, num_of_kernels, resolved);
+        std::cout<< "No cycle between kernels: "<< acyclic << std::endl;
+        for (auto& elem : resolved) {
+            std::cout<< elem <<" ";
+        }
+        std::cout<<std::endl;
+
+        return 0;
+    }
