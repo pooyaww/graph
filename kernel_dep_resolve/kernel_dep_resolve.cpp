79c135229c1a369dd47db9d25b4186d380adb4cf
Puya Amiri <pooya.w@gmail.com>
2019-07-19 22:55:03 +0200

    bug fix, stack, cycle detection but still minor problem in cycle report

--- kernel_dep_resolve/kernel_dep_resolve.cpp
+++ kernel_dep_resolve/kernel_dep_resolve.cpp
@@ -15,7 +15,7 @@ static bool is_free_kernel(const Dependencies& dependencies, const bool dependen
 
 // Get the kernels with no dependency (no input from other kernels to those)
 static size_t get_free_kernels(const Dependencies& dependencies, const bool dependency_bool_vector[],
-                               size_t num_of_kernels, std::stack<size_t>& free_kernels) {
+        size_t num_of_kernels, std::stack<size_t>& free_kernels) {
     for (size_t kernel = 0; kernel < num_of_kernels; ++kernel) {
         if (is_free_kernel(dependencies, dependency_bool_vector, kernel)) {
             free_kernels.push(kernel);
@@ -27,6 +27,8 @@ static size_t get_free_kernels(const Dependencies& dependencies, const bool depe
 bool topological_sort(Dependencies& dependencies, const size_t num_of_kernels, std::vector<size_t>& resolved) {
     std::stack<size_t> free_kernels;
     bool dependency_bool_vector[dependencies.size()];
+    size_t remaining_dependencies = dependencies.size();
+
 
     // in the begining all dependencies are marked
     for (size_t i = 0; i < dependencies.size(); ++i ) {
@@ -46,6 +48,8 @@ bool topological_sort(Dependencies& dependencies, const size_t num_of_kernels, s
         for (size_t i = 0; i < dependencies.size(); ++i) {
             if (dependency_bool_vector[i] && dependencies[i].first == free) {
                 dependency_bool_vector[i] = false;
+                remaining_dependencies--;
+
                 // Check if other kernels are free now
                 if (is_free_kernel(dependencies, dependency_bool_vector, dependencies[i].second)) {
                     // Add it to set of free kernels
@@ -56,25 +60,25 @@ bool topological_sort(Dependencies& dependencies, const size_t num_of_kernels, s
         num_of_free_kernels = free_kernels.size();
     }
     // if there is no more free kernels but there exist dependencies, a cycle is found
-    return dependencies.size() == 0;
+    return remaining_dependencies == 0;
 }
 
 int main(void) {
-    const size_t num_of_kernels = 2; // number of kernels
+    const size_t num_of_kernels = 4; // number of kernels
     Dependencies dependencies; // vector for kernel dependencies
     std::vector<size_t> resolved;
     // Dependencies should be determined by an analyse phase on kernels
     // Making dependencies (making a kernel of kernel dependencies)
     dependencies.emplace_back(0, 1);
-    dependencies.emplace_back(1, 0);
-    //dependencies.emplace_back(2, 0);
-    //dependencies.emplace_back(1, 2);
-    //dependencies.emplace_back(2, 3);
+    dependencies.emplace_back(1, 2);
+    dependencies.emplace_back(0, 2);
+    dependencies.emplace_back(2, 3);
+    dependencies.emplace_back(2, 0);
     //dependencies.emplace_back(3, 2);
 
     for (auto elem : dependencies)
         std::cout << elem.first << " ----> " << elem.second << std::endl;
-
+    std::vector<std::pair<size_t, size_t>> cycle;
     //Passing vector of dependencies
     if (topological_sort(dependencies, num_of_kernels, resolved)) {
         std::cout<< "No cycles between kernels"<< std::endl;
@@ -84,23 +88,15 @@ int main(void) {
         std::cout<<std::endl;
     } else {
         std::cout<< "Kernels have circular dependency"<< std::endl;
-        size_t index_i;
-        size_t index_j;
-//        for (auto pair1 : dependencies) {
-//            for (auto pair2 : dependencies) {
-//                if (pair1.first == pair2.second && pair2.first == pair1.second)
-//                    std::cout<< pair1.first<<"--->" << pair1.second<<std::endl;
-//                else
-                    for (size_t i = 0; i < dependencies.size(); ++i)
-                        for (size_t j = i; j < dependencies.size(); ++j)
-                            if (dependencies[i].first == dependencies[j].second) {
-                                index_i = i;
-                                index_j = j;
-                            }
-         //  }
-        }
-        std::cout<< dependencies[index_i].first << " <---> "  <<dependencies[index_j].first<< std::endl;
-   // }
+        for (size_t i = 0; i < dependencies.size(); ++i)
+            for (size_t j = i; j < dependencies.size(); ++j)
+                if (dependencies[i].first == dependencies[j].second) {
+                    cycle.emplace_back(i,j);
+                }
+    }
+    for (auto elem : cycle)
+        std::cout<< dependencies[elem.first].first << " <---> "  <<dependencies[elem.second].first<< std::endl;
+    // }
 
-    return 0;
-}
+        return 0;
+        }
